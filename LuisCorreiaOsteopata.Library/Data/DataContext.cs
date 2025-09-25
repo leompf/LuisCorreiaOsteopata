387efe391e49ed25bc29948c806a72ddcaba95ef
leompf <lmfraqueiro@gmail.com>
2025-09-25 21:16:28 +0100

    Add Users. Add Roles. Add Seeder. Add frontend template.

---
+++ LuisCorreiaOsteopata.Library/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using LuisCorreiaOsteopata.Library.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuisCorreiaOsteopata.Library.Data;
+
+public class DataContext : IdentityDbContext<User>
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+}
---
+++ LuisCorreiaOsteopata.Library/Data/SeedDB.cs
@@ -0,0 +1,54 @@
+﻿using LuisCorreiaOsteopata.Library.Data.Entities;
+using LuisCorreiaOsteopata.Library.Helpers;
+using Microsoft.AspNetCore.Identity;
+
+namespace LuisCorreiaOsteopata.Library.Data;
+
+public class SeedDB
+{
+    private readonly DataContext _context;
+    private readonly IUserHelper _userHelper;
+
+    public SeedDB(DataContext context, IUserHelper userHelper)
+    {
+        _context = context;
+        _userHelper = userHelper;
+    }
+
+    public async Task SeedAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+
+        await _userHelper.CheckRoleAsync("Utente");
+        await _userHelper.CheckRoleAsync("Colaborador");
+        await _userHelper.CheckRoleAsync("Administrador");
+
+        var user = await _userHelper.GetUserByEmailAsync("lmfraqueiro@gmail.com");
+
+        if (user == null)
+        {
+            user = new User
+            {
+                FirstName = "Leonardo",
+                LastName = "Fraqueiro",
+                Email = "lmfraqueiro@gmail.com",
+                UserName = "lmfraqueiro@gmail.com",
+            };
+
+            var result = await _userHelper.AddUserAsync(user, "123456");
+
+            if (result != IdentityResult.Success)
+            {
+                throw new InvalidOperationException("Não foi possível criar o utitlizador no Seeder");
+            }
+
+            await _userHelper.AddUserToRoleAsync(user, "Administrador");
+
+            var isInRole = await _userHelper.IsUserInRoleAsync(user, "Administrador");
+            if (!isInRole)
+            {
+                await _userHelper.AddUserToRoleAsync(user, "Administrador");
+            }
+        }
+    }
+}
